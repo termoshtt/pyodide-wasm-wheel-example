6c73a1888dc20285f0181cc8c0cc4d5c07403666
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-01-08 17:36:26 +0900

    setup nodejs example for Pyodide

---
+++ test.js
@@ -0,0 +1,10 @@
+const { loadPyodide } = require("pyodide");
+
+async function hello_python() {
+    let pyodide = await loadPyodide();
+    return pyodide.runPythonAsync("1+1");
+}
+
+hello_python().then((result) => {
+    console.log("Python says that 1+1 =", result);
+});
